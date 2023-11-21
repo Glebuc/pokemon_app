94cba39e3c38d30ec0047292e1bbe4838481fc26
Gleb Deryugin <gleb@mail.ru>
2023-11-21 18:06:23 +0300

    Edit ui file

---
+++ ui_function.py
@@ -0,0 +1,14 @@
+from ui_mainwindow import Ui_MainWindow
+
+from PySide6.QtCore import Slot
+from PySide6.QtWidgets import QPushButton, QMessageBox
+
+
+
+class UIFunction(Ui_MainWindow):
+    @Slot()
+    def get_search_field(self):
+
+
+
+
